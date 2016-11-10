c22e98bb8e643d8457b28ef3097cf0c7bf94b4c6
adamdossa <adam@inreachventures.com>
2016-11-10 13:15:16 +0000

    wait for job to finish executing
    wait for job to finish executing

--- setup.sh
+++ setup.sh
@@ -121,5 +121,6 @@ aws s3 cp s3://inreach-emr-jars/inreach-ml-batch.jar /root/spark/
 # Start streaming job
 echo "Kicking off spark job"
 nohup /root/spark-ec2/runJob.sh &
+wait
 
 popd > /dev/null
