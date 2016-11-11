d1d9846bf9e9a0231f0b7b169c37128e7d3561ed
adamdossa <adam@inreachventures.com>
2016-11-11 13:05:18 +0000

    Correct location for .pem file
    Correct location for .pem file

--- startScoringOnCluster.sh
+++ startScoringOnCluster.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./spark-ec2 --instance-type m4.xlarge --identity-file ../inreach-ml-batch/src/main/resources/spark-cluster.pem --key-pair spark-cluster --slaves 1 --region eu-west-1 --copy-aws-credentials --run-job /root/spark-ec2/runScoring.sh launch sparkScoring
+./spark-ec2 --instance-type m4.xlarge --identity-file ./clone/src/main/resources/spark-cluster.pem --key-pair spark-cluster --slaves 1 --region eu-west-1 --copy-aws-credentials --run-job /root/spark-ec2/runScoring.sh launch sparkScoring
