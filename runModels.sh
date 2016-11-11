c97750613c3dd58e54eb4af83514c473993a2ad2
adamdossa <adam@inreachventures.com>
2016-11-11 11:48:18 +0000

    Jobs for scoring and training
    Jobs for scoring and training

--- runModels.sh
+++
@@ -1,2 +0,0 @@
-#/bin/sh
-/root/spark/bin/spark-submit --class vc.inreach.ml.classifiers.DissertationTrainingTask --deploy-mode client --driver-memory 8g --driver-cores 2 /root/spark/inreach-ml-batch.jar -data_path s3n://inreach-prod-dynamo-data/dynamoDbData_20161103  -model_path s3n://inreach-ml-models/ -aws remote
---
+++ startTrainingOnCluster.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./spark-ec2 --instance-type m4.xlarge --identity-file ../inreach-ml-batch/src/main/resources/spark-cluster.pem --key-pair spark-cluster --slaves 2 --region eu-west-1 --copy-aws-credentials --ebs-vol-num 1 --ebs-vol-size 6 --run-job /root/spark-ec2/runTraining.sh launch sparkTraining
