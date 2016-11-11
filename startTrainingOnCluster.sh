d020383ece8755ce8b4b9169b0a0cd0c6ac565c6
adamdossa <adam@inreachventures.com>
2016-11-11 13:35:45 +0000

    fix scripts
    fix scripts

--- startTrainingOnCluster.sh
+++ startTrainingOnCluster.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./spark-ec2 --instance-type m4.xlarge --identity-file ./clone/src/main/resources/spark-cluster.pem --key-pair spark-cluster --slaves 2 --region eu-west-1 --copy-aws-credentials --ebs-vol-num 1 --ebs-vol-size 6 --run-job /root/spark-ec2/runTraining.sh launch sparkTraining
+./spark-ec2 --instance-type m4.xlarge --identity-file ../clone/src/main/resources/spark-cluster.pem --key-pair spark-cluster --slaves 2 --region eu-west-1 --copy-aws-credentials --ebs-vol-num 1 --ebs-vol-size 6 --run-job /root/spark-ec2/runTraining.sh launch sparkTraining
