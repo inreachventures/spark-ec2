48d1ecb3e06bea1fc9d2c9f04fc55eaeb84ccf56
adamdossa <adam@inreachventures.com>
2016-11-13 21:13:35 +0000

    Improve scoring jobs
    Improve scoring jobs

--- startScoringOnCluster.sh
+++ startScoringOnCluster.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./spark-ec2 --instance-type m4.xlarge --identity-file ../clone/src/main/resources/spark-cluster.pem --key-pair spark-cluster --slaves 1 --region eu-west-1 --copy-aws-credentials --master-instance-type t2.large --scoring launch sparkScoring
+./spark-ec2 --instance-type m4.xlarge --identity-file ../clone/src/main/resources/spark-cluster.pem --key-pair spark-cluster --slaves 1 --region eu-west-1 --copy-aws-credentials --scoring launch sparkScoring
