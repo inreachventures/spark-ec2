90e14c892c4f7bcd7c03322e9b5f60b722fe450b
adamdossa <adam@inreachventures.com>
2016-11-13 15:19:58 +0000

    Fix up scoring jobs for automatic deployment
    Fix up scoring jobs for automatic deployment

--- runScoring.sh
+++ runScoring.sh
@@ -1,2 +1,2 @@
 #/bin/sh
-/root/spark/bin/spark-submit --class vc.inreach.ml.scoring.SQSOrganizationScoringStream --deploy-mode client --driver-memory 8g --driver-cores 2 /root/spark/inreach-ml-batch.jar -model_path s3n://inreach-ml-models/models/ -aws remote
+/root/spark/bin/spark-submit --class vc.inreach.ml.scoring.SQSOrganizationScoringStream --deploy-mode client --driver-memory 4g --driver-cores 1 /root/spark/inreach-ml-batch.jar -model_path s3n://inreach-ml-models/models/ -aws remote
