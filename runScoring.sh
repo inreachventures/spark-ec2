a3897bd6263959cb401af58de216a527f7608a3d
adamdossa <adam@inreachventures.com>
2016-11-20 19:34:07 +0000

    use one executor w/ 4 cores
    use one executor w/ 4 cores

--- runScoring.sh
+++ runScoring.sh
@@ -1,2 +1,2 @@
 #/bin/sh
-/root/spark/bin/spark-submit --class vc.inreach.ml.scoring.SQSOrganizationScoringStream --deploy-mode client --driver-memory 8g --driver-cores 2 /root/spark/inreach-ml-batch.jar -model_path s3n://inreach-ml-models/ -aws remote -scoring_model OvrScoringModel
+/root/spark/bin/spark-submit --class vc.inreach.ml.scoring.SQSOrganizationScoringStream --deploy-mode client --num-executors 1 --executor-cores 4 --driver-memory 8g --driver-cores 2 /root/spark/inreach-ml-batch.jar -model_path s3n://inreach-ml-models/ -aws remote -scoring_model OvrScoringModel
