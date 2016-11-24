b1eb963f52b51c4579b973276b96d74671b95ccf
adamdossa <adam@inreachventures.com>
2016-11-24 12:26:53 +0000

    Fixes for training / scoring
    Fixes for training / scoring

--- runScoring.sh
+++ runScoring.sh
@@ -1,2 +1,2 @@
 #/bin/sh
-/root/spark/bin/spark-submit --class vc.inreach.ml.scoring.SQSOrganizationScoringStream --deploy-mode client --num-executors 1 --executor-cores 4 --driver-memory 5g --driver-cores 1 /root/spark/inreach-ml-batch.jar -model_path s3n://inreach-ml-models/ -aws remote -scoring_model OvrScoringModel
+/root/spark/bin/spark-submit --class vc.inreach.ml.scoring.SQSOrganizationScoringStream --deploy-mode client --driver-memory 5g --driver-cores 1 /root/spark/inreach-ml-batch.jar -model_path s3n://inreach-ml-models/ -aws remote -scoring_model OvrScoringModel
