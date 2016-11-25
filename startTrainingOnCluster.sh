3c0623ec4411eb041ea08f9856ff73376cb534c0
adamdossa <adam@inreachventures.com>
2016-11-25 12:34:44 +0000

    Use spot pricing
    Use spot pricing for training

--- startTrainingOnCluster.sh
+++ startTrainingOnCluster.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./spark-ec2 --instance-type m4.xlarge --identity-file ../clone/src/main/resources/spark-cluster.pem --key-pair spark-cluster --slaves 2 --region eu-west-1 --copy-aws-credentials --ebs-vol-num 1 --ebs-vol-size 12 --training launch sparkTraining_$RANDOM
+./spark-ec2 --instance-type m4.xlarge --spot-price 0.1 --identity-file ../clone/src/main/resources/spark-cluster.pem --key-pair spark-cluster --slaves 2 --region eu-west-1 --copy-aws-credentials --ebs-vol-num 1 --ebs-vol-size 12 --training launch sparkTraining_$RANDOM
