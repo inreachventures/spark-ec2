a393110c9362fb13a65cb6ffb3f11f26127e0086
adamdossa <adam@inreachventures.com>
2016-11-30 09:42:20 +0000

    Minimize training & scoring footprint
    Minimize training & scoring footprint

--- startScoringOnCluster.sh
+++ startScoringOnCluster.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./spark-ec2 --instance-type m4.xlarge --identity-file ../clone/src/main/resources/spark-cluster.pem --key-pair spark-cluster --slaves 1 --region eu-west-1 --copy-aws-credentials --scoring launch sparkScoring
+./spark-ec2 --instance-type t2.large --identity-file ../clone/src/main/resources/spark-cluster.pem --key-pair spark-cluster --slaves 1 --region eu-west-1 --copy-aws-credentials --scoring launch sparkScoring_$RANDOM
--- startTrainingOnCluster.sh
+++ startTrainingOnCluster.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./spark-ec2 --instance-type m4.xlarge --zone eu-west-1c --spot-price 0.1 --identity-file ../clone/src/main/resources/spark-cluster.pem --key-pair spark-cluster --slaves 2 --region eu-west-1 --copy-aws-credentials --ebs-vol-num 1 --ebs-vol-size 12 --training launch sparkTraining_$RANDOM
+./spark-ec2 --master-instance-type t2.large --instance-type m4.xlarge --zone eu-west-1c --spot-price 0.1 --identity-file ../clone/src/main/resources/spark-cluster.pem --key-pair spark-cluster --slaves 1 --region eu-west-1 --copy-aws-credentials --ebs-vol-num 1 --ebs-vol-size 4 --training launch sparkTraining_$RANDOM
