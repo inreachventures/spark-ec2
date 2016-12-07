11a43b726461672a18508ad919025706ae8efbee
adamdossa <adam@inreachventures.com>
2016-12-07 09:52:20 +0000

    Move to latest data
    Move to latest data

--- runTraining.sh
+++ runTraining.sh
@@ -1,2 +1,2 @@
 #/bin/sh
-/root/spark/bin/spark-submit --class vc.inreach.ml.classifiers.VsTrainingTask --deploy-mode client --driver-memory 8g --driver-cores 2 /root/spark/inreach-ml-batch.jar -data_path s3n://inreach-prod-dynamo-data/dynamoDbData/ -model_path s3n://inreach-ml-models/ -aws remote -most_recent 4500 -vs_classifier $CLASSIFIER
+/root/spark/bin/spark-submit --class vc.inreach.ml.classifiers.VsTrainingTask --deploy-mode client --driver-memory 8g --driver-cores 2 /root/spark/inreach-ml-batch.jar -data_path s3n://inreach-prod-dynamo-data/dynamoDbBackUp_20161203/ -model_path s3n://inreach-ml-models/ -aws remote -most_recent 4500 -vs_classifier $CLASSIFIER
