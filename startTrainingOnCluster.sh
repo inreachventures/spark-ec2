a918f6c558d6268b3dc96d848f05dcf52705fa8e
adamdossa <adam@inreachventures.com>
2017-03-01 14:35:45 +0000

    Bump spot price to 0.25

--- startTrainingOnCluster.sh
+++ startTrainingOnCluster.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./spark-ec2 --master-instance-type m4.xlarge --instance-type m4.xlarge --zone eu-west-1c --spot-price 0.1 --master-spot-price 0.1 --identity-file ../clone/inreach-ml-core/src/main/resources/spark-cluster.pem --key-pair spark-cluster --slaves 1 --region eu-west-1 --copy-aws-credentials --ebs-vol-num 1 --ebs-vol-size 12 --deploy-root-dir $JAR_DIR $1 launch sparkTraining_$RANDOM
+./spark-ec2 --master-instance-type m4.xlarge --instance-type m4.xlarge --zone eu-west-1c --spot-price 0.25 --master-spot-price 0.1 --identity-file ../clone/inreach-ml-core/src/main/resources/spark-cluster.pem --key-pair spark-cluster --slaves 1 --region eu-west-1 --copy-aws-credentials --ebs-vol-num 1 --ebs-vol-size 12 --deploy-root-dir $JAR_DIR $1 launch sparkTraining_$RANDOM
