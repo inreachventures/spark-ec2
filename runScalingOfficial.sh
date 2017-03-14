9c3190df8f203e9306cb3d1b8907dd747cae66fc
adamdossa <adam@inreachventures.com>
2017-03-14 10:52:50 +0000

    Move scaling to 0225

--- runScalingOfficial.sh
+++ runScalingOfficial.sh
@@ -1,2 +1,2 @@
 #/bin/sh
-/root/spark/bin/spark-submit --class vc.inreach.ml.training.classifiers.ScalingModelTask --deploy-mode client --driver-memory 14g --driver-cores 4 /jar/inreach-ml-training.jar -data_path s3n://inreach-prod-dynamo-data/dynamoDbData_20170218/ -model_path s3n://inreach-ml-models-official/ -aws remote -tag $CLUSTER_NAME:$CI_BRANCH
+/root/spark/bin/spark-submit --class vc.inreach.ml.training.classifiers.ScalingModelTask --deploy-mode client --driver-memory 14g --driver-cores 4 /jar/inreach-ml-training.jar -data_path s3n://inreach-prod-dynamo-data/dynamoDbData_20170225/ -model_path s3n://inreach-ml-models-official/ -aws remote -tag $CLUSTER_NAME:$CI_BRANCH
