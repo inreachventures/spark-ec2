6ef924f29132b57c34385babca6ab63a8ce9edd9
adamdossa <adam@inreachventures.com>
2017-05-02 15:37:30 +0100

    restart scoring process automatically

--- runScoring.sh
+++ runScoring.sh
@@ -1,2 +1,7 @@
 #/bin/sh
-/root/spark/bin/spark-submit --class vc.inreach.ml.scoring.streams.SQSOrganizationScoringStream --deploy-mode client --driver-memory 6g --driver-cores 2 /jar/inreach-ml-scoring.jar -model_path s3n://inreach-ml-models-official/ -aws remote -scoring_model VsScoringModel -tag $CLUSTER_NAME:$CI_BRANCH
+RC=0
+while [ $RC -eq 0 ]; do
+  echo "Starting scoring process"
+  /root/spark/bin/spark-submit --class vc.inreach.ml.scoring.streams.SQSOrganizationScoringStream --deploy-mode client --driver-memory 6g --driver-cores 2 /jar/inreach-ml-scoring.jar -model_path s3n://inreach-ml-models-official/ -aws remote -scoring_model VsScoringModel -tag $CLUSTER_NAME:$CI_BRANCH
+  RC=$?
+done
