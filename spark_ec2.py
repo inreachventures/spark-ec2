ff67fb585f2ce3517e2c6ce7d93799cd24449375
adamdossa <adam@inreachventures.com>
2017-05-08 12:45:34 +0100

    move to 2.1.1

--- spark_ec2.py
+++ spark_ec2.py
@@ -52,7 +52,7 @@ else:
     raw_input = input
     xrange = range
 
-SPARK_EC2_VERSION = "2.1.0"
+SPARK_EC2_VERSION = "2.1.1"
 SPARK_EC2_DIR = os.path.dirname(os.path.realpath(__file__))
 
 VALID_SPARK_VERSIONS = set([
@@ -82,7 +82,8 @@ VALID_SPARK_VERSIONS = set([
     "2.0.0",
     "2.0.1",
     "2.0.2",
-    "2.1.0"
+    "2.1.0",
+    "2.1.1"
 ])
 
 SPARK_TACHYON_MAP = {
