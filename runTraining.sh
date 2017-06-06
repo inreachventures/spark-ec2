1ce8f35d34405cc46168a3c28c5a864fc9b9c30e
adamdossa <adam@inreachventures.com>
2017-06-06 16:47:57 +0100

    but back cv

--- runTraining.sh
+++ runTraining.sh
@@ -1,2 +1,2 @@
 #/bin/sh
-/root/spark/bin/spark-submit --class vc.inreach.ml.training.classifiers.ClassifierTrainingTask --deploy-mode client --driver-memory 14g --driver-cores 4 /jar/inreach-ml-training.jar -data_path s3n://inreach-prod-dynamo-data/dynamoDbData_20170520/ -model_path s3n://inreach-ml-models/ -aws remote -classifier $CLASSIFIER -tag $CLUSTER_NAME:$CI_BRANCH -folds 5 -cross_validation none
+/root/spark/bin/spark-submit --class vc.inreach.ml.training.classifiers.ClassifierTrainingTask --deploy-mode client --driver-memory 14g --driver-cores 4 /jar/inreach-ml-training.jar -data_path s3n://inreach-prod-dynamo-data/dynamoDbData_20170520/ -model_path s3n://inreach-ml-models/ -aws remote -classifier $CLASSIFIER -tag $CLUSTER_NAME:$CI_BRANCH -folds 5 -cross_validation some
