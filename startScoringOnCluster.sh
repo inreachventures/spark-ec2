938c963bc5bf56fa164e708fa6cd4e4f59750dfc
Adam Dossa <adam.dossa@gmail.com>
2017-06-22 14:54:15 +0100

    Bump scorers up to t2.xlarge
    Bump scorers up to t2.xlarge

--- startScoringOnCluster.sh
+++ startScoringOnCluster.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./spark-ec2 --instance-type t2.large --identity-file ../clone/inreach-ml-core/src/main/resources/spark-cluster.pem --key-pair spark-cluster --slaves 0 --region eu-west-1 --copy-aws-credentials --deploy-root-dir $JAR_DIR --scoring launch sparkScoring_$RANDOM
+./spark-ec2 --instance-type t2.xlarge --identity-file ../clone/inreach-ml-core/src/main/resources/spark-cluster.pem --key-pair spark-cluster --slaves 0 --region eu-west-1 --copy-aws-credentials --deploy-root-dir $JAR_DIR --scoring launch sparkScoring_$RANDOM
