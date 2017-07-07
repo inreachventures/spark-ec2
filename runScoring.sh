9cdd6f9afdfb12262ce1756b81a6ca7ddee36554
adamdossa <adam@inreachventures.com>
2017-07-07 13:07:10 +0100

    Fix scoring tag

--- runScoring.sh
+++ runScoring.sh
@@ -2,6 +2,6 @@
 RC=0
 while [ $RC -eq 0 ]; do
   echo "Starting scoring process"
-  /root/spark/bin/spark-submit --conf spark.network.timeout=600s --class vc.inreach.ml.scoring.streams.SQSOrganizationScoringStream --deploy-mode client --driver-memory 54g --driver-cores 14 /jar/inreach-ml-scoring.jar -model_path s3n://inreach-ml-models-official/ -aws remote -scoring_model VsScoringModel -tag $CLUSTER_NAME:$CI_BRANCH
+  /root/spark/bin/spark-submit --conf spark.network.timeout=600s --class vc.inreach.ml.scoring.streams.SQSOrganizationScoringStream --deploy-mode client --driver-memory 54g --driver-cores 14 /jar/inreach-ml-scoring.jar -model_path s3n://inreach-ml-models-official/ -aws remote -scoring_model VsScoringModel -tag $CI_BRANCH
   RC=0
 done
