f394e461289c7ddddd8fb630ca3507abd6a49c1d
adamdossa <adam@inreachventures.com>
2017-08-01 16:10:12 +0100

    support official as well

--- startTestingOnCluster.sh
+++ startTestingOnCluster.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./spark-ec2 --instance-type m4.4xlarge --identity-file ../clone/inreach-ml-core/src/main/resources/spark-cluster.pem --key-pair spark-cluster --slaves 0 --region eu-west-1 --copy-aws-credentials --deploy-root-dir $JAR_DIR --test launch sparkTest_$RANDOM
+./spark-ec2 --instance-type m4.4xlarge --identity-file ../clone/inreach-ml-core/src/main/resources/spark-cluster.pem --key-pair spark-cluster --slaves 0 --region eu-west-1 --copy-aws-credentials --deploy-root-dir $JAR_DIR $1 launch sparkTesting_$RANDOM
