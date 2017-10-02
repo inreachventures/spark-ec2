56fec8e06b3aa923c96a2ada14677548ad2d9c73
Ghyslain <gaillard.ghyslain@gmail.com>
2017-10-02 10:39:37 +0100

    scale dynamoDbData_20170922

--- run_scale.sh
+++ run_scale.sh
@@ -5,7 +5,7 @@
                              --driver-memory 14g \
                              --driver-cores 4 \
                              /jar/inreach-ml-training.jar \
-                             -data_path s3n://inreach-prod-dynamo-data/dynamoDbData_20170904/ \
+                             -data_path s3n://inreach-prod-dynamo-data/dynamoDbData_20170922/ \
                              -aws $AWS \
                              -tag $CLUSTER_NAME:$CI_BRANCH \
                              -run_name $RUN_NAME
