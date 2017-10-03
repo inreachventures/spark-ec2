1d0f49c936eaddf4f4e496d97ec4af6390e0d313
adamdossa <adam@inreachventures.com>
2017-10-03 16:08:42 +0100

    kill boxes again

--- setup.sh
+++ setup.sh
@@ -133,6 +133,7 @@ if [ "$JOB_TYPE" != "" ]; then
   /root/spark-ec2/run_$JOB_TYPE.sh
   if [ $? != 0 ]; then
     yes | aws s3 cp /root/spark-ec2/nohup.out s3://inreach-ml-errors/$CLUSTER_NAME/
+    yes | /root/spark-ec2/spark-ec2 --region $REGION destroy $CLUSTER_NAME
   else
     yes | /root/spark-ec2/spark-ec2 --region $REGION destroy $CLUSTER_NAME
   fi
