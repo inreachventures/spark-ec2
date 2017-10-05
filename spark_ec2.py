0a5ce5f020a1f0b186ccb967c89c10c3b0e6f127
adamdossa <adam@inreachventures.com>
2017-10-05 11:31:15 +0100

    use build number instead

--- spark_ec2.py
+++ spark_ec2.py
@@ -829,11 +829,11 @@ def launch_cluster(conn, opts, cluster_name):
     time.sleep(15)
 
     # Give the instances descriptive names and set additional tags
-    ci_url = os.getenv('CI_BUILD_URL')
-    if ci_url is None:
-        ci_url = ""
+    ci_build_number = os.getenv('CI_BUILD_NUMBER')
+    if ci_build_number is None:
+        ci_build_number = ""
 
-    additional_tags = {"Cluster": cluster_name, "Type": cluster_name.split("_")[0], "CodeShip": ci_url}
+    additional_tags = {"Cluster": cluster_name, "Type": cluster_name.split("_")[0], "CodeShip": ci_build_number}
     if opts.additional_tags.strip():
         additional_tags = dict(
             map(str.strip, tag.split(':', 1)) for tag in opts.additional_tags.split(',')
