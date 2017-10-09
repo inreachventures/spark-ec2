eb2322a670faaf01ec81227ff24524cbb52fd6ce
Ghyslain <gaillard.ghyslain@gmail.com>
2017-10-09 15:03:47 +0100

    move back to 0409

--- run_trainp.sh
+++ run_trainp.sh
@@ -5,7 +5,7 @@
                              --driver-memory 40g \
                              --driver-cores 12 \
                              /jar/inreach-ml-training.jar \
-                             -data_path s3n://inreach-prod-dynamo-data/dynamoDbData_20170922/ \
+                             -data_path s3n://inreach-prod-dynamo-data/dynamoDbData_20170904/ \
                              -aws $AWS \
                              -tag $CLUSTER_NAME:$CI_BRANCH \
                              -run_name $RUN_NAME \
