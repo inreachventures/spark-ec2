44a3e615299c8ab2e072b7a4557be81f78ad57cd
Ghyslain <gaillard.ghyslain@gmail.com>
2017-10-30 12:41:15 +0000

    move to r3.4xlarge

--- startScoringOnCluster.sh
+++ startScoringOnCluster.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-./spark-ec2 --instance-type m4.4xlarge \
-            --zone eu-west-1a \
+./spark-ec2 --instance-type r3.4xlarge \
+            --zone eu-west-1c \
             --master-spot-price 0.3 \
             --identity-file ../clone/inreach-ml-core/src/main/resources/spark-cluster.pem \
             --key-pair spark-cluster \
