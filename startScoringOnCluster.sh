7e460fa06f23910a70d3faff0a7ba5ebf0e2e4ea
Ghyslain <gaillard.ghyslain@gmail.com>
2018-01-11 10:10:04 +0000

    move to 0.33 on r3.4xlarge

--- startScoringOnCluster.sh
+++ startScoringOnCluster.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ./spark-ec2 --instance-type r3.4xlarge \
             --zone eu-west-1c \
-            --master-spot-price 0.3 \
+            --master-spot-price 0.33 \
             --identity-file ../clone/inreach-ml-core/src/main/resources/spark-cluster.pem \
             --key-pair spark-cluster \
             --slaves 0 \
--- startTrainingOnCluster.sh
+++ startTrainingOnCluster.sh
@@ -2,8 +2,8 @@
 ./spark-ec2 --master-instance-type r3.4xlarge \
             --instance-type r3.4xlarge \
             --zone eu-west-1c \
-            --spot-price 0.3 \
-            --master-spot-price 0.3 \
+            --spot-price 0.33 \
+            --master-spot-price 0.33 \
             --identity-file ../clone/inreach-ml-core/src/main/resources/spark-cluster.pem \
             --key-pair spark-cluster \
             --slaves 4 \
